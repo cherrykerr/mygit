678268fef92f9a2e0d99381f2d028a4a1db1286b
cherrykerr <1829644791@qq.com>
2018-08-07 16:26:21 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import emitter from './event.js';
 
-function Square(props){  //�µĺ���С����
+
+/*function Square(props){
     return(
         <button className = "square" onClick={props.onClick}>
             {props.value}
         </button>
     );
+}*/
+
+
+class Square extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      x:this.props.x,
+      y:this.props.y
+    }
+  }
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
 }
-class Board extends React.Component { //����
+class Board extends React.Component { //棋盘
   constructor(props) {
       super(props);
       this.state = {
-          squares: Array(20).fill(Array(20).fill(null)),
+          squares: Array(20).fill(null).map(()=>Array(20).fill(null)), //包含“X”“O”null的二维数组
           xIsNext: true,
+          winner:false,
+          champion:undefined
       };
   }
+  componentDidMount   (){
+        let that = this;
+        this.eventEmitter = emitter.addListener("hello",(msg)=>{
+            that.setState({
+                squares:msg[0],
+                xIsNext:msg[1]
+            })
+        });
+    }
+    componentWillUnmount(){
+        emitter.removeListener(this.eventEmitter);
+    }
   renderSquare(x,y) {
       return <Square 
+        x={x}
+        y={y}
         value={this.state.squares[x][y]}
         onClick={() => this.handleClick(x,y)}
       />;
@@ -36,22 +75,36 @@ class Board extends React.Component { //����
   }
   handleClick(x,y){
       const squares = this.state.squares.slice();
-      console.log("x:"+x+" y:"+y);
+      if(squares[x][y] || this.state.winner){
+        return;
+      }
       squares[x][y] = this.state.xIsNext ? 'X' : 'O';
+      let winner = calculateWinner(this.state.squares,x,y);
+      if(winner){
+        this.setState({winner:true,champion:winner})
+      }
       this.setState({
         squares: squares,
-        xIsNext: !this.state.xIsNext,
+        xIsNext: !this.state.xIsNext
       });
+      emitter.emit("callMe",this.state);
   }
   render() {
     let templet = [];
     for(let i = 0 ; i < 20 ; i++){
         templet.push(this.renderRows(i));
     }
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    let status;
+    if (this.state.winner) {
+        status = 'Winner: ' + this.state.champion;
+    } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     return (
       <div>
-        <div className="status">{status}</div>
+          <div className="status">
+            {status}
+          </div>
           {templet}
       </div>
     );
@@ -59,19 +112,58 @@ class Board extends React.Component { //����
 }
 
 class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
+    constructor(){
+        super();
+        this.state = { 
+            history : [Array(20).fill(null).map(()=>Array(20).fill(null))]
+        }
+    }
+    componentDidMount   (){
+        let that = this;
+        this.eventEmitter = emitter.addListener("callMe",(msg)=>{
+            let history1 = that.state.history.slice();
+            history1.push(msg.squares.map((i)=>i.slice()));
+            that.setState({
+                msg : msg,
+                history : history1
+            })
+        });
+    }
+    componentWillUnmount(){
+        emitter.removeListener(this.eventEmitter);
+    }
+    jumpTo(move){
+        this.setState({
+            history:this.state.history.slice(0,move+1),
+            squares:this.state.history[move]
+        })
+        let a = (move+1)%2===0?false:true;
+        emitter.emit("hello",[this.state.history[move],a]);
+    }
+    render() {
+        console.log(this.state);
+        const moves = this.state.history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li>
+                <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+        return (
+        <div className="game">
+            <div className="game-board">
+                <Board/>
+            </div>
+            <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{moves}</ol>
+            </div>
         </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 // ========================================
@@ -81,22 +173,108 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
+function calculateWinner(squares,x,y) {
+  let count = 0;
+  let arr = [];
+  //第一种向下斜线
+  for(let i = 1 ; i < 5 ; i++){
+      if(x-i < 0 || y-i < 0){
+          break;
+      }
+      if(squares[x-i][y-i] === squares[x][y]){
+          count += 1;
+      }else{
+          break;
+      }
+  }
+  for(let i = 1 ; i < 5 ; i++){
+      if(x+i > 19 || y+i > 19){
+          break;
+      }
+      if(squares[x+i][y+i] === squares[x][y]){
+          count += 1;
+      }else{
+          break;
+      }
+  }
+  if(count === 4){
+      return squares[x][y];
+  }else{
+      count = 0;
+  };
+  //第二种向上斜线
+  for(let i = 1 ; i < 5 ; i++){
+      if(y-i < 0 || x+i > 19){
+          break;
+      }
+      if(squares[x+i][y-i] === squares[x][y]){
+          count += 1;
+      }else{
+          break;
+      }
+  }
+  for(let i = 1 ; i < 5 ; i++){
+      if(x-i < 0 || y+i > 19){
+          break;
+      }
+      if(squares[x-i][y+i] === squares[x][y]){
+          count += 1;
+      }else{
+          break;
+      }
+  }
+  if(count === 4){
+      return squares[x][y];
+  }else{
+      count = 0;
+  };
+  //第三种竖线
+  for(let i = 1 ; i < 5 ; i++){
+      if(x-i >= 0){
+          if(squares[x-i][y] === squares[x][y]){
+              arr.push(1);
+          }else{arr.push(0)}
+      }
+      if(x+i <= 19){
+          if(squares[x+i][y] === squares[x][y]){
+              arr.unshift(1);
+          }else{arr.unshift(0)}
+      }
+  }
+  for(let a = 0; a< arr.length; a++){
+      if(arr[a] === 1){
+          count += 1;
+      }else{
+          count = 0;
+      }
+      if(count === 4){
+          return squares[x][y];
+      }
+  }
+  count = 0;
+  arr = [];
+  //第四种横线
+  for(let i = 1 ; i < 5 ; i++){
+      if(y-i >= 0){
+          if(squares[x][y-i] === squares[x][y]){
+              arr.push(1);
+          }else{arr.push(0)}
+      }
+      if(y+i <= 19){
+          if(squares[x][y+i] === squares[x][y]){
+              arr.unshift(1);
+          }else{arr.unshift(0)}
+      }
+  }
+  for(let a = 0; a< arr.length; a++){
+      if(arr[a] === 1){
+          count += 1;
+      }else{
+          count = 0;
+      }
+      if(count === 4){
+          return squares[x][y];
+      }
   }
   return null;
 }
